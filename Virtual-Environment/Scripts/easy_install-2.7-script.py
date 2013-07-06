e1bb9589974769d7b203af24d6ae16a5f7ddd4fb
OMID EBRAHIMI <ebrahimi.omid69@gmail.com>
2013-07-06 12:42:24 +0430

    V1-Upgrade Packages

--- Virtual-Environment/Scripts/easy_install-2.7-script.py
+++ Virtual-Environment/Scripts/easy_install-2.7-script.py
@@ -1,10 +1,10 @@
 #!H:\Web-Programming\Python\Practices\Books\Virtual-Environment\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'distribute==0.6.27','console_scripts','easy_install-2.7'
-__requires__ = 'distribute==0.6.27'
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.8','console_scripts','easy_install-2.7'
+__requires__ = 'setuptools==0.8'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('distribute==0.6.27', 'console_scripts', 'easy_install-2.7')()
+        load_entry_point('setuptools==0.8', 'console_scripts', 'easy_install-2.7')()
     )
--- Virtual-Environment/Scripts/pip-2.7-script.py
+++ Virtual-Environment/Scripts/pip-2.7-script.py
@@ -1,10 +1,10 @@
 #!H:\Web-Programming\Python\Practices\Books\Virtual-Environment\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.1','console_scripts','pip-2.7'
-__requires__ = 'pip==1.1'
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.3.1','console_scripts','pip-2.7'
+__requires__ = 'pip==1.3.1'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('pip==1.1', 'console_scripts', 'pip-2.7')()
+        load_entry_point('pip==1.3.1', 'console_scripts', 'pip-2.7')()
     )
